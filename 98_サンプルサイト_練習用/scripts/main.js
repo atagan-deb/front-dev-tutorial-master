d83417e6726582cc8b008da0256031c545b88929
atagan-deb <yusei.deb@gmail.com>
2022-02-23 09:57:31 +0900

    Flexboxを使ったレスポンシブ画面の実装の復習！！（Popularセクションの作成）まで受講

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hero = new HeroSlider('.swiper-container');
-    hero.start();
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const hero = new HeroSlider(".swiper-container");
+  hero.start();
 
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
+    }
+  };
+
+  const so = new ScrollObserver(".tween-animate-title", cb);
+
+  const _inviewAnimation = (el, inview) => {
+    if (inview) {
+      el.classList.add("inview");
+    } else {
+      el.classList.remove("inview");
+    }
+  };
+  const so2 = new ScrollObserver(".cover-slide", _inviewAnimation);
+});
